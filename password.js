770ba3eac2e1bdcb4b80669fcc8066fb30bb35e8
Brett Stevens <brettstevens899@yahoo.com>
2022-02-11 11:08:22 -0700

    first commit

---
+++ password.js
@@ -0,0 +1,36 @@
+console.log("welcome to the password validator tool")
+console.log("Please enter password to validate")
+let password = ("Ilovetocode") 
+if(password.length > 10){
+    console.log("success")
+} else {
+    console.log("failure")
+}
+
+// I can't figure out how to check for other things, for the extra credit.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                                                                                            
+                                                                                        
+console.log(".d8888. db    db  .o88b.  .o88b. d88888b .d8888. .d8888.")
+console.log("88'  YP 88    88 d8P  Y8 d8P  Y8 88'     88'  YP 88'  YP ")
+console.log("`8bo.   88    88 8P      8P      88ooooo `8bo.   `8bo.  ")
+console.log("  `Y8b. 88    88 8b      8b      88~~~~~   `Y8b.   `Y8b.")
+console.log("db   8D 88b  d88 Y8b  d8 Y8b  d8 88.     db   8D db   8D ")
+console.log("`8888Y' ~Y8888P'  `Y88P'  `Y88P' Y88888P `8888Y' `8888Y' ")
+
+
+//ASCII art for extra credit 
---
+++ sldc.js
@@ -0,0 +1,26 @@
+// In this section you will answer some work through problems related to the Software Development Lifecycle
+
+//////////////////PROBLEM 1////////////////////
+/*
+    Reorganize the following steps of the Software Development Lifecycle to be in their correct order.
+*/
+
+// Analysis of Requirements
+// Design
+// Implementation
+// Maintain
+// Planning
+// Testing & Integration
+
+
+//////////////////PROBLEM 2////////////////////
+/*
+    Create a variable for each of the above steps of the Software Development Lifecycle and set the values of each variable to a definition of the step.
+    The first one is done for you. 
+*/
+const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added." 
+const analysisofRequirements = " the process of determining user expectations for a new or modified product"
+const design = "The Design phase models the way a software application will work"
+const implementation = "Implementation includes, installation of hardware, installation of software onto production computers."
+const maintain = " software upgrades, repairs, and fixes of the software if it breaks"
+const testingIntergration = "Integration testing encompasses all aspects of a software system's performance, functionality and reliability."
